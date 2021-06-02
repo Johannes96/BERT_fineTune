d422c0c9f046f1d20f7a5d28299d29188dc83d66
Johannes96 <j-sach@t-online.de>
2021-06-02 16:29:32 +0200

    Darstelung asc performance

--- Darstellungen/performance_asc.py
+++ Darstellungen/performance_asc.py
@@ -2,55 +2,26 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-# %% scatter asc
+# %% data
 url = 'https://raw.githubusercontent.com/Johannes96/BERT_fineTune/master/data/finetuning_data.csv'
 data_finetune = pd.read_csv(url, sep=';')
 
 acc = data_finetune['accuracy'].tolist()
-f1 = data_finetune['f1-score'].tolist()
+f1 = data_finetune['f1-score'].tolist() # erstetze mit f1-micro
 mse = data_finetune['mse'].tolist()
 labels = data_finetune['Modell'].tolist()
+# %% accuracy und f1-score asc
 
-# %%
-fig, ax1 = plt.subplots()
-
-color = '#8aab33'
-ax1.set_xlabel('Epochs')
-ax1.set_ylabel(ylabel='', color=color)
-ax1.plot(x, y_acc, color=color, label='Accuracy')
-ax1.plot(x, y_mse, color=color, linestyle='--', label='MSE')
-# plt.plot(x, y_eff, color=color, linestyle=':', label='Efficiency')
-ax1.tick_params(axis='y', labelcolor=color)
-plt.legend(loc='upper left')
-plt.xticks(x, x)
-
-ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
-color = 'tab:blue'
-ax2.set_ylabel('Trainingszeit [min]', color=color)  # we already handled the x-label with ax1
-ax2.plot(x, y_time_m, color=color)
-ax2.tick_params(axis='y', labelcolor=color)
-fig.tight_layout()  # otherwise the right y-label is slightly clipped
-
-plt.show()
-
-# %%
-
-# data to plot
+# create figure
 n_groups = 6
-
-# create plot
 fig, ax1 = plt.subplots()
 index = np.arange(n_groups)
 bar_width = 0.35
 # opacity = 0.8
 
-rects1 = plt.bar(index, acc, bar_width,
-color='g',
-label='Accuracy')
+rects1 = plt.bar(index, acc, bar_width, color='g', label='Accuracy')
 
-rects2 = plt.bar(index + bar_width, f1, bar_width,
-color='#8aab33',
-label='F1-Score')
+rects2 = plt.bar(index + bar_width, f1, bar_width, color='#8aab33', label='F1-Score')
 
 # plt.xlabel('Sprachmodell')
 plt.ylabel('Performance')
@@ -58,10 +29,27 @@ plt.title('Performace beim Fine-Tuning der Aspekt Sentiment Klassifikation')
 plt.xticks(index + bar_width, labels)
 plt.legend()
 
-ax2 = ax1.twinx()
-
 plt.tight_layout()
 plt.show()
+# plt.savefig('/home/johannes/Dropbox/Praktisches Seminar/Darstellungen/ft_performance_asc_v1.png', dpi=300)
 
-# %%
-print(type(labels))
+# %% mse asc
+n_groups = 6
+fig, ax1 = plt.subplots()
+index = np.arange(n_groups)
+bar_width = 0.35
+
+rects1 = plt.bar(index, mse, bar_width, color='#8aab33', label='mse')
+
+# plt.xlabel('Sprachmodell')
+plt.ylabel('Mean Squared Error')
+plt.title('Performace beim Fine-Tuning der Aspekt Sentiment Klassifikation')
+plt.xticks(index, labels) # bei zwei Balken: index + bar_width
+
+plt.tight_layout()
+# plt.show()
+plt.savefig('/home/johannes/Dropbox/Praktisches Seminar/Darstellungen/ft_performance_asc_mse_v1.png', dpi=300)
+# %% accuracy und f1-score ae
+
+# %% mse ae
+# ggf. mse von asc und ae in einer Darstellung kombinieren
