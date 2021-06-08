9385ff8fb870065429a3e965f68484dc4ef68ae7
Johannes96 <j-sach@t-online.de>
2021-06-08 16:19:03 +0200

    ae Daten v1

--- Darstellungen/efficiency_scatter_finetuning.py
+++ Darstellungen/efficiency_scatter_finetuning.py
@@ -27,3 +27,25 @@ plt.show()
 # plt.savefig('/home/johannes/Dropbox/Praktisches Seminar/Darstellungen/finetuning_effizienz_asc.png', dpi=300)
 
 # %% scatter ae
+url = 'https://raw.githubusercontent.com/Johannes96/BERT_fineTune/master/data/finetuning_data_ae.csv'
+data_finetune_ae = pd.read_csv(url, sep=';')
+
+labels2 = data_finetune_ae['Modell']
+acc_asc = data_finetune_ae['accuracy']
+time_asc = data_finetune_ae['trainingtime in s'] / 60
+x_max = time_asc.max() + 1
+x_min = time_asc.min()
+
+plt.scatter(time_asc, acc_asc, label='GLUE', alpha=0.7, c='#8aab33')
+
+plt.xlabel('Trainingszeit [min]')
+plt.ylabel('Performance [accuracy]')
+# plt.title('finetuning Effizienz - ASC')
+plt.xlim(x_max, 0) # erst max weil weniger Zeit besser ist
+# plt.ylim(y_min - 0.2, y_max + 0.2)
+
+for i, label in enumerate(labels2):
+    plt.annotate(label, (time_asc[i], acc_asc[i]))
+
+plt.show()
+# plt.savefig('/home/johannes/Dropbox/Praktisches Seminar/Darstellungen/finetuning_effizienz_ae_acc.png', dpi=300)
