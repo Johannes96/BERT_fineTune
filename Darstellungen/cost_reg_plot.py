3d7eb18c0be4d4708aec7e8646319f81a275dbc8
Johannes96 <j-sach@t-online.de>
2021-06-13 16:21:43 +0200

    reg_plot v1

--- Darstellungen/cost_reg_plot.py
+++ Darstellungen/cost_reg_plot.py
@@ -1,2 +1,17 @@
 import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
 
+url = 'https://raw.githubusercontent.com/Johannes96/BERT_fineTune/master/data/cost_reg_plot.csv'
+data = pd.read_csv(url, sep=',')
+
+data_asc = data.drop(['ft_cost_ae'], axis=1)
+data_asc.dropna(inplace=True)
+
+data_ae = data.drop(['ft_cost_asc'], axis=1)
+data_ae.dropna(inplace=True)
+
+sns.regplot(x='ft_cost_asc', y='pt_cost', data=data_asc)
+plt.show()
+
+# Ergebnis --> kein Zusammenhang
